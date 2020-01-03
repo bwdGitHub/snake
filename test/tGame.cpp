c7561856b7841ce232afdb5e5d24bcb34600f73b
Ben Wells-Day <benwellsday@gmail.com>
2020-01-03 01:10:31 +0000

    test render doesn't error

--- test/tGame.cpp
+++ test/tGame.cpp
@@ -20,6 +20,14 @@ TEST(Game,Int){
     EXPECT_EQ(game.width,w);
 }
 
+TEST(render,NoExcept){    
+    auto game = Game(1,1);
+    // TODO:
+    // This prints to std::cout
+    // It'd be nice to instead be able to construct game with an arbitrary output buffer.
+    EXPECT_NO_THROW(game.render());
+}
+
 int main(int argc, char** argv){
     ::testing::InitGoogleTest(&argc,argv);
     return RUN_ALL_TESTS();
