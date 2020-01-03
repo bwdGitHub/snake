ed1f30d5988510f6d131148402a9b1bb592b5528
Ben Wells-Day <benwellsday@gmail.com>
2020-01-03 03:41:19 +0000

    use render format from tutorial

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,7 +4,10 @@
 
 enum class ScreenCode
 {
-    EMPTY
+    EMPTY,
+    VERT_WALL,
+    HORIZ_WALL,
+    CORNER
 };
 
 char renderMap(ScreenCode c)
@@ -17,6 +20,16 @@ char renderMap(ScreenCode c)
     {
     case ScreenCode::EMPTY:
         return ' ';
+
+    case ScreenCode::VERT_WALL:
+        return '|';
+
+    case ScreenCode::HORIZ_WALL:
+        return '-';
+
+    case ScreenCode::CORNER:
+        return '+';
+
     default:
         return ' ';
     }
@@ -24,14 +37,31 @@ char renderMap(ScreenCode c)
 
 Game::Game(unsigned int h, unsigned int w):height{h},width{w}
 {   
-    // Initialize "screen" with EMPTY    
+    // Initialize "screen" with EMPTY
     for (auto row = 0; row < height; row++)
     {
         std::vector<ScreenCode> column;
         screen.push_back(column);
+        bool isRowEdge = (row==0)||(row==height-1);
         for (auto col = 0; col < width; col++)
         {
-            screen[row].push_back(ScreenCode::EMPTY);
+            // TODO:
+            // This is ugly, got to be a better way.
+            bool isColEdge = (col==0)||(col==width-1);
+            bool isCorner = isRowEdge&&isColEdge;
+            ScreenCode c = ScreenCode::EMPTY;
+            if(isCorner){
+                c = ScreenCode::CORNER;
+            }
+            else{
+                if(isColEdge){
+                    c = ScreenCode::VERT_WALL;
+                }
+                if(isRowEdge){
+                    c = ScreenCode::HORIZ_WALL;
+                }
+            }
+            screen[row].push_back(c);
         }
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,25 @@ class Snake {};
 
 class Apple {};
 
+Game instantiate(int argc, char** argv){
+    // TODO
+    // Validate inputs 
+    // bad inputs will be problematic down the line
+    // e.g. a negative int seems to be readable here 
+    // Does it become a massive unsigned int by overflow
+    // Or just remain negative?
+    // Instead - just ban it.
+    unsigned int height = 8;
+    unsigned int width = 16;
+    if(argc==3){
+        height = std::stoi(argv[1]);
+        width = std::stoi(argv[2]);
+    }
+    return Game(height,width);
+}
+
 int main(int argc, char** argv){
-    Game game = Game(10,15);   
+    Game game = instantiate(argc,argv);
     game.render();
     return 0;
 }
