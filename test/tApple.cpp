a022c309ef97c97c3ba7bb369389e978308e7846
Ben Wells-Day <benwellsday@gmail.com>
2020-01-05 04:46:37 +0000

    tests for Apple including some artifacts of trying out things. Will probably remove those in the next commit.

--- test/tApple.cpp
+++ test/tApple.cpp
@@ -1,6 +1,55 @@
 #include <gtest/gtest.h>
 #include "Apple.hpp"
 
+class ConstructorTest :
+    public testing::TestWithParam<std::pair<int,int>>{};
+
+TEST_P(ConstructorTest, positionSet){
+    // A parameterized test example.
+    Apple apple = Apple(GetParam());
+    EXPECT_EQ(apple.position,GetParam());
+}
+
+// Parameterized test suites have to be instantiated.
+// Note the parameter values here cover cases we might not even want to work (0,-1)
+// But for now they pass.
+INSTANTIATE_TEST_SUITE_P(ConstructorTestSuite,
+    ConstructorTest,
+    testing::Values(std::make_pair(1,1),std::make_pair(-1,1),std::make_pair(0,1),std::make_pair(100,200)));
+
+// Test fixtures are classes. 
+// TEST_F that specify these classes can access them as child classes.
+
+Apple globalApple = Apple(std::make_pair(1,1));
+
+class AppleFixture : public ::testing::Test{
+    // If it is complex to setup the data for tests, a fixture can help.
+    // But the fixture will construct each time, so
+    // If it is heavy to setup the fixture, you may need other tricks.
+    // One trick could be to create the object once and reference it,
+    // but then the state of that object can be modified by various tests.
+    // In general that's probably confusing.
+    protected:
+        Apple apple = Apple(std::make_pair(1,1));
+        Apple& appleRef = globalApple;
+};
+
+TEST_F(AppleFixture, positionIsSetPublic){
+    auto currentPos = apple.position;
+    auto newPos = std::make_pair(currentPos.first+1,currentPos.second-1);
+    apple.position = newPos;
+    ASSERT_NE(currentPos,newPos);
+    EXPECT_EQ(apple.position,newPos);
+}
+
+TEST_F(AppleFixture, breakReferenceState){
+    appleRef.position = std::make_pair(0,0);
+}
+
+TEST_F(AppleFixture, referenceStateChanged){
+    std::cout << appleRef.position.first << "\n";
+}
+
 int main(int argc, char** argv){
     ::testing::InitGoogleTest(&argc,argv);
     return RUN_ALL_TESTS();
