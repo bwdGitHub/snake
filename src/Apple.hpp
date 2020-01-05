567595677a4f24210c432979940317e6024f8c50
Ben Wells-Day <benwellsday@gmail.com>
2020-01-05 05:19:01 +0000

    add comments on Apple template

--- src/Apple.hpp
+++ src/Apple.hpp
@@ -2,6 +2,12 @@
 #define APPLE_H
 #include <utility>
 
+// By using a template here, in principle someone can use different implementations of Point.
+// A minor downside is that all Apple calls have to be Apple<>
+
+// TODO:
+// It feels like this would be more robust if an interface was provided by me,
+// currently it is implicit that because std::pair<int,int> is the default, your Point should be similar.
 template<typename Point=std::pair<int,int>>
 class Apple {
     public:
