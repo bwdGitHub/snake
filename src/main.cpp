d45293ca6a9b0c154b84d953d801d01e5e49b621
Ben Wells-Day <benwellsday@gmail.com>
2020-01-22 23:12:00 +0000

    factor out some of the start screen logic

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <curses.h>
 #include <cstring>
 
-Game instantiate(int argc, const char** argv){
+Game instantiate(const int argc, const char** argv){
     // TODO
     // Validate inputs 
     // bad inputs will be problematic down the line
@@ -28,31 +28,65 @@ WINDOW* initializeGameWindow(int h,int w,double delay){
     return win;
 }
 
-int main(const int argc, const char** argv){
-    // Make argc and argv const so I can re-instantiate game later and know it'll be in the start state.
-    // For now just pass a window to the game, and see if I can quit from there
-    Game game = instantiate(argc,argv);
-    // Standard initialization
-    //initializeCurses();
+enum class ColorPair{
+    // Convenience enum to relate the colours initialized in 
+    // initCurses() to the windows they're intended for.
+    // Though these have to be static_cast<short> anyway...
+    Game=1,
+    Score=2
+};
+
+void initCurses(){
+    // Initialize conditions for the curses terminal.
     initscr();
     clear();
     noecho();
     cbreak();
     curs_set(0);
     start_color();
-    init_pair(1,COLOR_GREEN,COLOR_BLACK);
-    init_pair(2,COLOR_WHITE,COLOR_BLUE);
+    init_pair(static_cast<short>(ColorPair::Game),COLOR_GREEN,COLOR_BLACK);
+    init_pair(static_cast<short>(ColorPair::Score),COLOR_WHITE,COLOR_BLUE);
+}
 
-    WINDOW* startWin = newwin(5,game.width,0,0);
+WINDOW* startWindow(int width, int yStart, int xStart){
+    // Initialize the window to show the start screen.
+    // Don't take a height - it needs to be at least 5,
+    // could allow height input and assert but no reason to yet.
+    WINDOW* startWin = newwin(5,width,yStart,xStart);
     mvwprintw(startWin,0,0,"SNAKE");
     mvwprintw(startWin,1,0,"'e' - Start");
     mvwprintw(startWin,2,0,"'wasd' - Move");
     mvwprintw(startWin,3,0,"'r' - Restart");
     mvwprintw(startWin,4,0,"'q' - Quit");
-    wbkgd(startWin,COLOR_PAIR(1));
+    wbkgd(startWin,COLOR_PAIR(static_cast<short>(ColorPair::Game)));
     wrefresh(startWin);
-    char initialInput = wgetch(startWin);
-    if(initialInput=='e'){
+    return startWin;
+}
+
+bool startScreenWait(WINDOW* win, char startChar, char quitChar){
+    // Wait for valid input from user.
+    // This is a bit of a weird pattern, this while loop can only end if valid input is pressed
+    // Since there's only two choices we only need to return if one was pressed
+    bool validInput = false;
+    bool startGame = false;
+    while(!validInput){
+        char input = wgetch(win);
+        startGame = input==startChar;
+        validInput = startGame || input==quitChar;
+    }
+    return startGame;
+}
+
+int main(const int argc, const char** argv){
+    // Make argc and argv const so I can re-instantiate game later and know it'll be in the start state.
+
+    Game game = instantiate(argc,argv);
+
+    initCurses();
+
+    WINDOW* startWin = startWindow(game.width,0,0);
+    bool startGame = startScreenWait(startWin,'e','q');
+    if(startGame){
         wclear(startWin);
         wrefresh(startWin);
         delwin(startWin);
@@ -107,23 +141,6 @@ int main(const int argc, const char** argv){
         }
     }
     refresh();
-    endwin();
-
-    // TODO
-    // Switch this on as a fallback from input args?
-    // bool quit = false;
-    // while(!quit){
-    //     game.render();
-    //     // TODO:
-    //     // This is a hacky way to listen for inputs.
-    //     // curses/ncurses is a library that's meant to be good for this.
-    //     char key = getch();
-    //     if(key=='q'){
-    //         quit = true;
-    //     }
-    //     else{
-    //         game.update(key);
-    //     }
-    // }    
+    endwin();   
     return 0;
 }
