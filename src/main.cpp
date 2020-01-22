b2295fddb9d0a14bb049f6d7c22dd284774e7fbc
Ben Wells-Day <benwellsday@gmail.com>
2020-01-22 02:11:56 +0000

    more code to refactor - added a start screen. This is functionally everything I wanted to do, but things could look a bit nicer both frontend and backend, and in principle I should do that before merging to master (despite doing this on my own). Also my idealism for testing seems to be purely idealism at this stage, but thinking about that should help modularize the pieces

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,6 @@ WINDOW* initializeGameWindow(int h,int w,double delay){
     keypad(win,TRUE);
     box(win,0,0);
     wrefresh(win);
-    curs_set(0);
     wtimeout(win,delay);
     return win;
 }
@@ -42,60 +41,75 @@ int main(const int argc, const char** argv){
     clear();
     noecho();
     cbreak();
+    curs_set(0);
     start_color();
     init_pair(1,COLOR_GREEN,COLOR_BLACK);
     init_pair(2,COLOR_WHITE,COLOR_BLUE);
 
+    WINDOW* startWin = newwin(5,game.width,0,0);
+    mvwprintw(startWin,0,0,"SNAKE");
+    mvwprintw(startWin,1,0,"'e' - Start");
+    mvwprintw(startWin,2,0,"'wasd' - Move");
+    mvwprintw(startWin,3,0,"'r' - Restart");
+    mvwprintw(startWin,4,0,"'q' - Quit");
+    wbkgd(startWin,COLOR_PAIR(1));
+    wrefresh(startWin);
+    char initialInput = wgetch(startWin);
+    if(initialInput=='e'){
+        wclear(startWin);
+        wrefresh(startWin);
+        delwin(startWin);
 
-    double delay = 100;
-    if(argc>3){
-        // TODO
-        // The delay is not used if a char is picked up by wgetch
-        // This makes it non-uniform, which is noticeable at slow speed.
-        delay = std::stod(argv[3]);
-    }
-    WINDOW* win = initializeGameWindow(game.height,game.width,delay);
-    WINDOW* scoreWin = newwin(3,game.width,game.height,0);
-    wbkgd(win,COLOR_PAIR(1));
-    wbkgd(scoreWin,COLOR_PAIR(2));
-    wrefresh(scoreWin);
-    char scoreStr[] = "Score: ";
-    char score[3];
-    mvwprintw(scoreWin,0,0,scoreStr);
-    wrefresh(scoreWin);
-
-
-
-    bool cursesQuit = false;    
-    bool hasLost = false;
-    while(!cursesQuit){
-        char input = wgetch(win);
-        cursesQuit = input=='q';
-        if(input=='r'){
-            // reset
-            hasLost = false;
-            game = instantiate(argc,argv);
-            // Reset the score window here.
-            // Currently undecided if it's better to have Game do all rendering
-            // Or just use what we know from Game to update for some stuff.
-            wclear(scoreWin);
-            mvwprintw(scoreWin,0,0,scoreStr);
-            wrefresh(scoreWin);
-        }     
-        if(!hasLost){
-            hasLost = game.update(input);
-            sprintf(score,"%d",game.score);
-            mvwprintw(scoreWin,0,strlen(scoreStr)+1,score);
-            wrefresh(scoreWin);
-            game.cursesRender(win,input); 
+        double delay = 100;
+        if(argc>3){
+            // TODO
+            // The delay is not used if a char is picked up by wgetch
+            // This makes it non-uniform, which is noticeable at slow speed.
+            delay = std::stod(argv[3]);
         }
-        if(hasLost){
-            wclear(scoreWin);
-            mvwprintw(scoreWin,0,0,"You Lose!");
-            mvwprintw(scoreWin,1,0,"'r' - Restart");
-            mvwprintw(scoreWin,2,0,"'q' - Quit");
-            wrefresh(scoreWin);
-        }                        
+        WINDOW* win = initializeGameWindow(game.height,game.width,delay);
+        WINDOW* scoreWin = newwin(3,game.width,game.height,0);
+        wbkgd(win,COLOR_PAIR(1));
+        wbkgd(scoreWin,COLOR_PAIR(2));
+        wrefresh(scoreWin);
+        char scoreStr[] = "Score: ";
+        char score[3];
+        mvwprintw(scoreWin,0,0,scoreStr);
+        wrefresh(scoreWin);
+
+
+
+        bool cursesQuit = false;    
+        bool hasLost = false;
+        while(!cursesQuit){
+            char input = wgetch(win);
+            cursesQuit = input=='q';
+            if(input=='r'){
+                // reset
+                hasLost = false;
+                game = instantiate(argc,argv);
+                // Reset the score window here.
+                // Currently undecided if it's better to have Game do all rendering
+                // Or just use what we know from Game to update for some stuff.
+                wclear(scoreWin);
+                mvwprintw(scoreWin,0,0,scoreStr);
+                wrefresh(scoreWin);
+            }     
+            if(!hasLost){
+                hasLost = game.update(input);
+                sprintf(score,"%d",game.score);
+                mvwprintw(scoreWin,0,strlen(scoreStr)+1,score);
+                wrefresh(scoreWin);
+                game.cursesRender(win,input); 
+            }
+            if(hasLost){
+                wclear(scoreWin);
+                mvwprintw(scoreWin,0,0,"You Lose!");
+                mvwprintw(scoreWin,1,0,"'r' - Restart");
+                mvwprintw(scoreWin,2,0,"'q' - Quit");
+                wrefresh(scoreWin);
+            }                        
+        }
     }
     refresh();
     endwin();
